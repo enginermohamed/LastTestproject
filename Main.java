e6191a1d88236f1a152b40c009359a5facdb6a66
Mohamed Isack <engmohan99@gmail.com>
2024-07-21 06:52:01 +0300

    added some java file

---
+++ Main.java
@@ -0,0 +1,7 @@
+// java is the first programming that i coded 
+// and made my first project 
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+    }
+  }
