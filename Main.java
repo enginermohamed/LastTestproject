47e9941bf060691141e706779a1c3d506c55d4b9
Mohamed Isack <engmohan99@gmail.com>
2024-07-21 07:17:56 +0300

    added css

--- Main.java
+++
@@ -1,7 +0,0 @@
-// java is the first programming that i coded 
-// and made my first project 
-public class Main {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
-    }
-  }
